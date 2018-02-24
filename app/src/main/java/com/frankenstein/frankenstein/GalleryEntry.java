773ffb460a658961f3d8c070a37a91593515d6a0
Timothy Qiu <jia.xi.qiu.20@dartmouth.edu>
2018-02-24 11:54:35 -0500

    GalleryEntryCreated

---
+++ app/src/main/java/com/frankenstein/frankenstein/GalleryEntry.java
@@ -0,0 +1,91 @@
+package com.frankenstein.frankenstein;
+
+import java.sql.Blob;
+
+/**
+ * Created by Timothyqiu on 2018-02-24.
+ */
+
+public class GalleryEntry {
+    private long entryId;
+    private String userEmail;
+    private long postTime;
+    private float azimuth;
+    private float latitude;
+    private float longitude;
+    private String postText;
+    private Blob picture;
+    private String summary;
+
+    public long getEntryId() {
+        return entryId;
+    }
+
+    public void setEntryId(long entryId) {
+        this.entryId = entryId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public long getPostTime() {
+        return postTime;
+    }
+
+    public void setPostTime(long postTime) {
+        this.postTime = postTime;
+    }
+
+    public float getAzimuth() {
+        return azimuth;
+    }
+
+    public void setAzimuth(float azimuth) {
+        this.azimuth = azimuth;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getPostText() {
+        return postText;
+    }
+
+    public void setPostText(String text) {
+        this.postText = text;
+    }
+
+    public Blob getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Blob picture) {
+        this.picture = picture;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+}
